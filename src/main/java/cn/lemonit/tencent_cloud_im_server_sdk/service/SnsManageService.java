b3a69e895e67ea47940b92e989e453925b78ccdd
renshujulao <y5chao@aliyun.com>
2020-12-18 00:04:43 +0800

    feat:删除好友

--- src/main/java/cn/lemonit/tencent_cloud_im_server_sdk/service/SnsManageService.java
+++ src/main/java/cn/lemonit/tencent_cloud_im_server_sdk/service/SnsManageService.java
@@ -3,14 +3,10 @@ package cn.lemonit.tencent_cloud_im_server_sdk.service;
 
 import cn.lemonit.tencent_cloud_im_server_sdk.IMClient;
 import cn.lemonit.tencent_cloud_im_server_sdk.exceptions.ServiceException;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.ReqBlackListAdd;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.ReqFriendAdd;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.RespBlackListAdd;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.RespFriendAdd;
+import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.*;
 
 public class SnsManageService extends CommonService {
     private static final String SERVICE_NAME = "sns";
-    private static final String COMMAND_PREFIX = "COMMAND";
 
     private final IMClient imClient;
 
@@ -20,12 +16,19 @@ public class SnsManageService extends CommonService {
 
     //添加好友
     public static final String COMMAND_FRIEND_ADD = "friend_add";
+    public static final String COMMAND_FRIEND_DELETE = "friend_delete";
+
     //添加黑名单
-    public static final String BLACK_LIST_ADD = COMMAND_PREFIX+ "black_list_add";
+    public static final String BLACK_LIST_ADD = "black_list_add";
 
     public RespFriendAdd friendAdd(ReqFriendAdd reqFriendAdd) throws ServiceException {
         String url = imClient.createUrl(SERVICE_NAME, COMMAND_FRIEND_ADD);
         return post(url, RespFriendAdd.class, reqFriendAdd);
+    }
+
+    public RespFriendDelete friendDelete(ReqFriendDelete reqFriendDelete) throws ServiceException {
+        String url = imClient.createUrl(SERVICE_NAME, COMMAND_FRIEND_DELETE);
+        return post(url,RespFriendDelete.class,reqFriendDelete);
     }
 
     public RespBlackListAdd blackListAdd(ReqBlackListAdd reqBlackListAdd) throws ServiceException {
--- src/test/java/cn/lemonit/tencent_cloud_im_server_sdk/service/SnsManageServiceTest.java
+++ src/test/java/cn/lemonit/tencent_cloud_im_server_sdk/service/SnsManageServiceTest.java
@@ -2,13 +2,12 @@ package cn.lemonit.tencent_cloud_im_server_sdk.service;
 
 import cn.hutool.json.JSONUtil;
 import cn.lemonit.tencent_cloud_im_server_sdk.Common;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.AddFriendItem;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.ReqFriendAdd;
-import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.RespFriendAdd;
+import cn.lemonit.tencent_cloud_im_server_sdk.model.sns.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -31,6 +30,22 @@ class SnsManageServiceTest {
                     .ForceAddFlags(1)
                     .build();
             RespFriendAdd result = Common.createClient().getSnsManageService().friendAdd(reqFriendAdd);
+            System.out.println("请求结果：" + JSONUtil.toJsonStr(result));
+        });
+    }
+
+
+
+    @Test
+    @DisplayName("删除好友")
+    void FriendDelete() {
+        assertDoesNotThrow(() -> {
+            ReqFriendDelete reqFriendDelete = ReqFriendDelete.builder()
+                    .To_Account(Arrays.asList("54","55"))
+                    .From_Account("73")
+                    .DeleteType("Delete_Type_Single")
+                    .build();
+            RespFriendDelete result = Common.createClient().getSnsManageService().friendDelete(reqFriendDelete);
             System.out.println("请求结果：" + JSONUtil.toJsonStr(result));
         });
     }
